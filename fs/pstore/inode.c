6fbcff9cff184ced056f3a5f50a7c40ef51e347e
ShedrockN4 <william.argoud@laposte.net>
2017-04-25 00:51:30 +0200

    Add pmsg V4 (Mark Salyzyn)
    A secured user-space accessible pstore object. Writes
    to /dev/pmsg0 are appended to the buffer, on reboot
    the persistent contents are available in
    /sys/fs/pstore/pmsg-ramoops-[ID].
    
    One possible use is syslogd, or other daemon, can
    write messages, then on reboot provides a means to
    triage user-space activities leading up to a panic
    as a companion to the pstore dmesg or console logs.
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    
    v2: switch from snprintf to scnprintf
    v3: split out prz_ok checking into PATCH 3/5
        replace pmsg_lseek with noop_llseek
        use pr_fmt() macro
        make write atomic and use a vmalloc'd bounce buffer
    v4: use mutex_lock instead of spin_lock.

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -326,6 +326,9 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 	case PSTORE_TYPE_MCE:
 		sprintf(name, "mce-%s-%lld", psname, id);
 		break;
+	case PSTORE_TYPE_PMSG:
+		sprintf(name, sizeof(name), "pmsg-%s-%lld", psname, id);
+		break;
 	case PSTORE_TYPE_UNKNOWN:
 		sprintf(name, "unknown-%s-%lld", psname, id);
 		break;
